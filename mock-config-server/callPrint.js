abcffe008edfb859863b0d3c522374dcc390d917
VBruilo <74017273+VBruilo@users.noreply.github.com>
2025-06-19 10:44:32 +0200

    Pass IDs to print startup

--- mock-config-server/callPrint.js
+++ mock-config-server/callPrint.js
@@ -3,7 +3,10 @@ import axios from 'axios';
 
 async function callPrint() {
   try {
-    const res = await axios.post('http://localhost:3000/api/prints/parameterized/coin');
+    const res = await axios.post('http://localhost:3000/api/prints/parameterized/coin', {
+      machineConfigID: 'example-machine',
+      configSetID: 'example-config'
+    });
     console.log(res.data);
   } catch (err) {
     console.error(err.message);
